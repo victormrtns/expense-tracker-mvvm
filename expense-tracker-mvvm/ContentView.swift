d86be89582c07384f5a0f7c4521a8e2ef6de3ab8
victormrtns <vmhugo00@gmail.com>
2025-04-04 16:31:37 -0300

    Initial Commit

---
+++ expense-tracker-mvvm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  expense-tracker-mvvm
+//
+//  Created by Victor Hugo Martins on 04/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ expense-tracker-mvvm/expense_tracker_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_tracker_mvvmApp.swift
+//  expense-tracker-mvvm
+//
+//  Created by Victor Hugo Martins on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct expense_tracker_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
